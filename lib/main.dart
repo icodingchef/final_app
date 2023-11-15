4f76f5975932fe4f9db67797904a03d41405b9e0
icodingchef <icodingchef@gmail.com>
2023-11-15 09:29:32 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  int initialValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Final Variable"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Count(counter: initialValue),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(
+                  () {
+                    initialValue++;
+                  },
+                );
+              },
+              child: const Text('increase'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Count extends StatelessWidget {
+  const Count({super.key, required this.counter});
+
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Counter: $counter',
+      style: const TextStyle(fontSize: 25, color: Colors.red),
+    );
+  }
+}
