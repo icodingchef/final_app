673b256629d30c7c9c14cd1bc763c8884ccd6e0b
icodingchef <icodingchef@gmail.com>
2023-12-12 15:50:22 +0900

    revised

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
+        colorSchemeSeed: Colors.purple,
       ),
       home: const MyPage(),
     );
